8e87057505668644cb406e035b99adc2dde278b9
NamPham <vannam1329@gmail.com>
2021-10-01 15:30:35 +0700

    edit view

---
+++ admin/layout/footer.php
@@ -0,0 +1,27 @@
+<!-- Jquery JS-->
+<script src="../../frontend/homepage/vendor/jquery-3.2.1.min.js"></script>
+    <!-- Bootstrap JS-->
+    <script src="../../frontend/homepage/vendor/bootstrap-4.1/popper.min.js"></script>
+    <script src="../../frontend/homepage/vendor/bootstrap-4.1/bootstrap.min.js"></script>
+    <!-- ../../frontend/homepage/Vendor JS       -->
+    <script src="../../frontend/homepage/vendor/slick/slick.min.js">
+    </script>
+    <script src="../../frontend/homepage/vendor/wow/wow.min.js"></script>
+    <script src="../../frontend/homepage/vendor/animsition/animsition.min.js"></script>
+    <script src="../../frontend/homepage/vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
+    </script>
+    <script src="../../frontend/homepage/vendor/counter-up/jquery.waypoints.min.js"></script>
+    <script src="../../frontend/homepage/vendor/counter-up/jquery.counterup.min.js">
+    </script>
+    <script src="../../frontend/homepage/vendor/circle-progress/circle-progress.min.js"></script>
+    <script src="../../frontend/homepage/vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <script src="../../frontend/homepage/vendor/chartjs/Chart.bundle.min.js"></script>
+    <script src="../../frontend/homepage/vendor/select2/select2.min.js">
+    </script>
+
+    <!-- Main JS-->
+    <!-- <script src="../../frontend/homepage/js/main.js"></script> -->
+
+</body>
+
+</html>
---
+++ admin/layout/sidebar.php
@@ -0,0 +1,38 @@
+<aside class="menu-sidebar">
+            <div class="logo">
+                <a href="/webdemo/">
+                    <img src="../../frontend/homepage/images/icon/logo.png" alt="Cool Admin" />
+                </a>
+            </div>
+            <div class="menu-sidebar__content">
+                <nav class="navbar-sidebar">
+                <ul class="list-unstyled navbar__list">
+                        <li class="has-sub">
+                            <a  href="/webdemo/" class="">
+                            <i class="fas fa-home"></i></i>Home
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/webdemo/admin/user/listUser/">
+                            <i class="fas fa-user"></i></i>Account</a>
+                        </li>
+                        <li>
+                            <a href="/webdemo/admin/product/listProduct/">
+                            <i class="fas fa-box"></i>Products</a>
+                        </li>
+                        <li>
+                            <a href="/webdemo/admin/category/listCategory/">
+                            <i class="fas fa-clipboard-list"></i>Category</a>
+                        </li>
+                        <li>
+                            <a href="/webdemo/admin/order/listOrder/">
+                                <i class="fas fa-calendar-alt"></i>Order</a>
+                        </li>
+                        <li>
+                            <a href="/webdemo/admin/role/listRole/">
+                                <i class="fas fa-map-marker-alt"></i>Role</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </aside>
